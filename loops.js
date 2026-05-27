c4a60cadabf5acb3894c256960475119599e3efc
KrishendreeX <Krishendree@gmail.com>
2026-05-27 11:41:40 +0200

    initial commit

---
+++ loops.js
@@ -0,0 +1,24 @@
+// TODO: Create an array called numbers with values 1 through 5
+let numbers = [1, 2, 3, 4, 5];
+
+// TODO: Write a for loop that prints each number in the array
+console.log("for numbers");
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+
+// TODO: Write a while loop that counts down from 5 to 1
+console.log("While loop countdown: ");
+let count = 5;
+while (count > 0) {
+    console.log(count);
+    count--;
+}
+
+
+
+
+
+// Krishendree - finshed creating the arrays, and count down of loops.
+// Double checked and everything works perfectly.
+// Owam can you please help create and even count and sum everything up.
